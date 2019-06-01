4bc8768218ee4bf79a9e7b46395a2283e845c3a9
Miguel Nistal <nistam328@gmail.com>
2019-06-01 16:42:21 -0400

    Added a const lookup table for Fish data so we can indentify if something is being fished by ID

--- Fishage.lua
+++ Fishage.lua
@@ -26,8 +26,24 @@ end
 -- Events
 -- 
 Fishage.events = {}
+Fishage.eventHistory = {}
 Fishage.events["CHAT_MSG_LOOT"] = function(self, event, ...)
     local text = select(1, ...) 
     local playerName = select(2, ...)
-    SendSystemMessage(" P="..playerName.." T="..text)
+    local playerName2 = select(5, ...)
+    local itemId = string.match(text, ".*item:(%w+)::")
+    local itemName = string.match(text, "[%w+]")
+    SendSystemMessage(" ID="..itemId.." name="..itemName)
+    table.insert(Fishage.eventHistory, text)
+
+    -- Check if its a fish
+    if (FISHAGE_FISH_DATA[itemId] ~= nil) then
+        SendSystemMessage("Its a fish! "..FISHAGE_FISH_DATA[itemId]["Name"])
+    end
+end
+
+Fishage.events["LOOT_ITEM_AVAILABLE"] = function(self, event, ...)
+    local itemTooltip = select(1, ...)
+    local lootHandle = select(2, ...)
+    SendSystemMessage(" Tip="..itemTooltip.." lH="..lootHandle)
 end
---
+++ FishageFishData.lua
@@ -0,0 +1,8 @@
+
+
+FISHAGE_FISH_DATA = {}
+FISHAGE_FISH_DATA["6291"] = { Name = "Raw Brilliant Smallfish",  ItemLevel = 5  }
+FISHAGE_FISH_DATA["6303"] = { Name = "Raw Slitherskin Mackerel", ItemLevel = 5  }
+FISHAGE_FISH_DATA["6361"] = { Name = "Raw Rainbow Fin Albacore", ItemLevel = 15 }
+FISHAGE_FISH_DATA["6317"] = { Name = "Raw Loch Frenzy",          ItemLevel = 15 }
+FISHAGE_FISH_DATA["6289"] = { Name = "Raw Longjaw Mud Snapper",  ItemLevel = 15 }
