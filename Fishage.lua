90bce27900ffe8bb988f12606b0553bbedd397e9
Miguel Nistal <nistam328@gmail.com>
2019-06-01 15:16:51 -0400

    Added event handling infrastructure, registered for CHAT_MSG_LOOT since thats where we'll pick up our info

--- Fishage.lua
+++ Fishage.lua
@@ -1,7 +1,33 @@
 
-
+--
+-- Addon Initialization
+-- 
 Fishage = {};
+Fishage.RegisterEvents = function(self) 
+    for event_name, callback in pairs(Fishage.events) do
+        self:RegisterEvent(event_name)
+        SendSystemMessage(event_name)
+    end
+end
 
 Fishage.OnLoad = function(self)
     SendSystemMessage("Fishage 1.0 Loaded")
+    Fishage.RegisterEvents(self)
+end
+
+Fishage.OnEvent = function(self, event, ...)
+    SendSystemMessage(event)
+    if (Fishage.events[event] ~= nil) then
+        Fishage.events[event](self, event, ...)
+    end
+end
+
+-- 
+-- Events
+-- 
+Fishage.events = {}
+Fishage.events["CHAT_MSG_LOOT"] = function(self, event, ...)
+    local text = select(1, ...) 
+    local playerName = select(2, ...)
+    SendSystemMessage(" P="..playerName.." T="..text)
 end
